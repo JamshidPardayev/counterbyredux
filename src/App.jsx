ab0752e432ec80a480cacc07ef0c0b6288c4c848
JamshidPardayev <jamshidnt2024@gmail.com>
2025-04-14 18:54:52 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useDispatch, useSelector } from "react-redux"
+import { decrement, increment, incrementByAmount, decrementByAmount } from "./redux/counterSlice";
+
+function App() {
+  const dispatch = useDispatch();
+  const count = useSelector((state) => state.counter.value);
+
+  return (
+    <div className="text-center">
+      <h2>{count}</h2>
+      <button onClick={() => dispatch(increment()) }>Oshirish</button>    
+      <button onClick={() => dispatch(decrement()) }>Kamaytirish</button>    
+      <button onClick={() => dispatch(incrementByAmount(10)) }>incrementByAmount</button>    
+      <button onClick={() => dispatch(decrementByAmount(10)) }>decrementByAmount</button>    
+    </div>
+  )
+}
+
+export default App
